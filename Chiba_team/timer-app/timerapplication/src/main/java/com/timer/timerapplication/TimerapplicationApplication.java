46c127085b75a0774e97ad55f6a0e63878dfc1be
unknown <itanoLocal@P1704.beerhall.candd.co.jp>
2023-04-14 12:07:52 +0900

    maven project create.

---
+++ Chiba_team/timer-app/timerapplication/src/main/java/com/timer/timerapplication/TimerapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.timer.timerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimerapplicationApplication.class, args);
+	}
+
+}
---
+++ Chiba_team/timer-app/timerapplication/src/test/java/com/timer/timerapplication/TimerapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timer.timerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimerapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
